e218c2376829169b05c2fa3bdc42421c4f7a8b2e
Jonas Nylund <35763296+jonasnylund@users.noreply.github.com>
2025-10-29 16:24:53 +0100

    Byt till 2 blanksteg indentering

--- eslint.config.js
+++ eslint.config.js
@@ -6,37 +6,37 @@ import css from '@eslint/css';
 import {defineConfig} from 'eslint/config';
 
 export default defineConfig([
-    {
-        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-        plugins: {js},
-        extends: ['js/recommended'],
-        languageOptions: {globals: globals.browser},
-        rules: {
-            "semi": ['error', 'always'],
-            'max-len': [
-                'error',
-                {
-                    code: 80,
-                    tabWidth: 4,
-                    comments: 80,
-                    ignoreUrls: true,
-                    ignoreRegExpLiterals: true,
-                },
-            ],
-            'indent': ['error', 4],
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: {js},
+    extends: ['js/recommended'],
+    languageOptions: {globals: globals.browser},
+    rules: {
+      semi: ['error', 'always'],
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          tabWidth: 2,
+          comments: 80,
+          ignoreUrls: true,
+          ignoreRegExpLiterals: true,
         },
+      ],
+      indent: ['error', 2],
     },
-    tseslint.configs.recommended,
-    {
-        files: ['**/*.md'],
-        plugins: {markdown},
-        language: 'markdown/gfm',
-        extends: ['markdown/recommended'],
-    },
-    {
-        files: ['**/*.css'],
-        plugins: {css},
-        language: 'css/css',
-        extends: ['css/recommended'],
-    },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.md'],
+    plugins: {markdown},
+    language: 'markdown/gfm',
+    extends: ['markdown/recommended'],
+  },
+  {
+    files: ['**/*.css'],
+    plugins: {css},
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
 ]);
